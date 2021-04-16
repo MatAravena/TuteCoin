72a987367f6a1b0984d932980f94f67e5f3f8436
Matias Aravena <matias.aravena.g@gmail.com>
2021-04-16 12:44:43 +0200

    Add post to recieve a new Block in the blockchain

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require("body-parser");
+
 const BlockChain = require("./blockChain");
 
 const app = express();
 const blockchain = new BlockChain();
 
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+
 app.get("/api/blocks", (req, res) => {
     res.json(blockchain.chain);
+});
+
+app.post("/api/mine", (req, res) => {
+
+    const { data } = req.body;
+
+    blockchain.addBlock({ data });
+
+    res.redirect("/api/blocks");
+
 });
 
 const PORT = 5500
